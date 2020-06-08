844a69033001db814971511a2952cad42a3ee04a
Jin <oliveira.jinbok@gmail.com>
2020-06-08 16:15:14 -0300

    logon page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,11 @@
 import React from 'react';
-import './App.css';
+import Logon from './pages/Logon'
+import './global.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-         Hello World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Logon />
     </div>
   );
 }
---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { FiLogIn }from 'react-icons/fi'
+import './styles.css'
+import heroImage from '../../assets/heroes.png'
+import heroLogo from '../../assets/logo.svg'
+
+
+export default function Logon() {
+  return (
+    <div className="logon-container">
+      <section className="form">
+        <img src={heroLogo} alt="Be The Hero"/>
+        
+        <form>
+          <h1>Faça seu Logon</h1>
+          <input placeholder="Sua ID"/>
+          <button className="button" type="submit">Entrar</button>
+
+          <a href="/register">
+            <FiLogIn size={16} color="#E02041"/>
+            Não tenho cadastro
+          </a>
+        </form>
+      </section>
+      <img src={heroImage} alt="Heroes"/>
+    </div>
+  )
+}
